f75d42d5b187cd0ef4da2c259dbf24b744d42e08
RaviPratihast <ravipratihast@gmail.com>
2023-03-09 12:07:11 +0530

    Add: Pages and its Routes along with its links.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 // import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { Home, Products, Wishlist, Cart, Login } from "./Pages/index";
 
 function App() {
   return (
     <div className="App">
-      <>hi</>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/productListing">Products</Link>
+        <Link to="/wishlist">Wishlist</Link>
+        <Link to="/wishlist">Cart</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productListing" element={<Products />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Products from "./Products";
+import Wishlist from "./Wishlist";
+import Cart from "./Cart";
+import Login from "./Login";
+
+export { Home, Products, Wishlist, Cart, Login };
