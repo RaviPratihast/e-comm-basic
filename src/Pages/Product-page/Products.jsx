60f91541e88d654d9c50a3362b399d60d744d7bf
RaviPratihast <ravipratihast@gmail.com>
2023-03-12 10:32:52 +0530

    Add: Added side filter container & Refactor style.

--- src/Pages/Product-page/Products.jsx
+++ src/Pages/Product-page/Products.jsx
@@ -4,16 +4,26 @@ import "./Product.css";
 function Products() {
   const { state, dispatch } = useProduct();
   return (
-    <div className="products-container">
-      {state.product.map((items) => {
-        return (
-          <div key={items.id} className="item-container">
-            <h1>{items.name}</h1>
-            {items.inStock ? <p> Price: {items.price}</p> : <p>Out Of Stock</p>}
-          {items.inStock &&   <button>Add To Cart</button>}
-          </div>
-        );
-      })}
+    <div className="filter-product-container">
+      
+      <div className="filters-container">
+        
+      </div>
+      <div className="products-container">
+        {state.product.map((items) => {
+          return (
+            <div key={items.id} className="item-container">
+              <h1>{items.name}</h1>
+              {items.inStock ? (
+                <p> Price: {items.price}</p>
+              ) : (
+                <p>Out Of Stock</p>
+              )}
+              {items.inStock && <button>Add To Cart</button>}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
